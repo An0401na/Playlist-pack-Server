57ce1892d05fe36e43f3fe0d68c712dcbca134ca
ulsandonghun <donghoon141@naver.com>
2023-12-04 19:43:33 +0900

    [fix] : cors 오류 수정

--- src/main/java/com/example/Playlist_pack/Controller/UserController.java
+++ src/main/java/com/example/Playlist_pack/Controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.Playlist_pack.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
