aea9de1c071af4e00f2f1dceda49e21a526d7178
안나 <dksek3050@gmail.com>
2023-12-13 07:05:07 +0900

    Fix: Global Exception Respnse 형식 변경

--- src/main/java/com/example/Playlist_pack/Global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/Playlist_pack/Global/exception/handler/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
-package com.example.Playlist_pack.Global.exception;
+package com.example.Playlist_pack.Global.exception.handler;
 
 import com.example.Playlist_pack.Global.dto.response.ErrorResponseDto;
+import com.example.Playlist_pack.Global.dto.response.HttpResponse;
 import com.example.Playlist_pack.Global.exception.custom.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -14,22 +16,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponseDto> businessExceptionHandle(BusinessException e) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public HttpResponse<ErrorResponseDto> businessExceptionHandle(BusinessException e) {
         log.warn("businessException : {}", e);
-        return ResponseEntity.internalServerError()
+        return HttpResponse.status(e.getHttpStatus())
                 .body(ErrorResponseDto.from(e.getHttpStatus(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDto> allUncaughtHandle(Exception e) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public HttpResponse allUncaughtHandle(Exception e) {
         log.error("allUncaughtHandle : {}", e);
-        return ResponseEntity.internalServerError().build();
+        return HttpResponse.internalServerErrorBuild(e);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
         log.error("methodArgumentNotValidException : {}", e);
-        return ResponseEntity.badRequest()
+        return HttpResponse.badRequest()
                 .body(ErrorResponseDto.from(HttpStatus.BAD_REQUEST, e.getFieldErrors().get(0).getDefaultMessage()));
     }
 }
