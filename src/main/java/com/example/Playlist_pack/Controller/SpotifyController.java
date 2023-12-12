9721d149642d6e3723e4603ba707c525e50baea0
안나 <dksek3050@gmail.com>
2023-12-12 22:36:16 +0900

    Fix: 트랙 이름으로 노래 조회시 Response 형식 변경

--- src/main/java/com/example/Playlist_pack/Controller/SpotifyController.java
+++ src/main/java/com/example/Playlist_pack/Controller/SpotifyController.java
@@ -1,16 +1,18 @@
 package com.example.Playlist_pack.Controller;
 
 import com.example.Playlist_pack.Dto.SpotifySearchResponseDto;
+import com.example.Playlist_pack.Global.dto.response.HttpResponse;
 import com.example.Playlist_pack.Service.SpotifyService;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -19,11 +21,15 @@ public class SpotifyController {
     SpotifyService spotifyService =new SpotifyService();
 
     @GetMapping("/search/{trackname}")
-    public List<SpotifySearchResponseDto>searchTracksByTrackname(@PathVariable String trackname) throws IOException, ParseException, SpotifyWebApiException {
-
-
-        return spotifyService.SearchByTrackname(trackname);
+    public HttpResponse<List<SpotifySearchResponseDto>> searchTracksByTrackname(@PathVariable String trackname) throws IOException, ParseException, SpotifyWebApiException {
+        return HttpResponse.okBuild(spotifyService.SearchByTrackname(trackname));
     }
+
+    //기존 작성해둔 코드 입니다 ~ 나중에 지워주세요
+//    @GetMapping("/search/{trackname}")
+//    public List<SpotifySearchResponseDto>searchTracksByTrackname(@PathVariable String trackname) throws IOException, ParseException, SpotifyWebApiException {
+//        return spotifyService.SearchByTrackname(trackname);
+//    }
 
     @GetMapping("/search/{artist}/{trackname}")
     public List<SpotifySearchResponseDto> searchTracksByTracknameAndArtist(@PathVariable String trackname,@PathVariable String artist) throws IOException, ParseException, SpotifyWebApiException {
