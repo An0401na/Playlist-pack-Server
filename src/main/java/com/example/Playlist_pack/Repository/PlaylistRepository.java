4c580d4b4b2b211aa912994072655b90765989de
안나 <dksek3050@gmail.com>
2023-12-14 05:35:14 +0900

    Fix: 불필요한 부분 삭제

--- src/main/java/com/example/Playlist_pack/Repository/PlaylistRepository.java
+++ src/main/java/com/example/Playlist_pack/Repository/PlaylistRepository.java
@@ -3,9 +3,6 @@ package com.example.Playlist_pack.Repository;
 import com.example.Playlist_pack.Domain.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-    Optional<Playlist> findByPlaylistPack(PlaylistPack playlistPack);
 
 }
