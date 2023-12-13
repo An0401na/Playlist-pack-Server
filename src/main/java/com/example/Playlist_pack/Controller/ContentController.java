3b7acf28dcc348bbe1140416a483851689b21894
ulsandonghun <donghoon141@naver.com>
2023-12-13 18:34:43 +0900

    [fix] : CORS 오류 해결

--- src/main/java/com/example/Playlist_pack/Controller/ContentController.java
+++ src/main/java/com/example/Playlist_pack/Controller/ContentController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("contents")
