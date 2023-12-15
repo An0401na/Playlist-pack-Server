a032576bb1c8b0684c5abf5fb514e2a45d2c6d6c
안나 <dksek3050@gmail.com>
2023-12-15 23:02:00 +0900

    Fix: 선물 박스 전체 조회시 편지 내용과 노래 정보까지 포함하도록 수정

--- src/main/java/com/example/Playlist_pack/Dto/response/PlaylistResponseDto.java
+++ src/main/java/com/example/Playlist_pack/Dto/response/PlaylistResponseDto.java
@@ -1,21 +1,36 @@
 package com.example.Playlist_pack.Dto.response;
 
 import com.example.Playlist_pack.Domain.Playlist;
+import com.example.Playlist_pack.Dto.SpotifySearchResponseDto;
 import lombok.Builder;
+
+import java.time.LocalDateTime;
 
 @Builder
 public record PlaylistResponseDto(Long playlistId,
                                   String coverIdx,
                                   String decoIdx,
                                   String colorIdx,
-                                  String spotifyId) {
-    public static PlaylistResponseDto from(Playlist playlist){
+                                  String title,
+                                  String artistName,
+                                  String imageUrl,
+                                  String previewUrl,
+                                  String friendname,
+                                  String letter,
+                                  LocalDateTime createdDate ) {
+    public static PlaylistResponseDto of(Playlist playlist, SpotifySearchResponseDto spotifySearchResponseDto){
         return PlaylistResponseDto.builder()
                 .playlistId(playlist.getPlaylistId())
                 .coverIdx(playlist.getCoverIdx())
                 .decoIdx(playlist.getDecoIdx())
                 .colorIdx(playlist.getColorIdx())
-                .spotifyId(playlist.getSpotifyId())
+                .title(spotifySearchResponseDto.getTitle())
+                .artistName(spotifySearchResponseDto.getArtistName())
+                .imageUrl(spotifySearchResponseDto.getImageUrl())
+                .previewUrl(spotifySearchResponseDto.getPreviewUrl())
+                .friendname(playlist.getFriendname())
+                .letter(playlist.getLetter())
+                .createdDate(playlist.getCreatedDate())
                 .build();
 
     }
