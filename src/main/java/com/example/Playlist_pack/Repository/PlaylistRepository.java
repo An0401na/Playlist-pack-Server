16efdbb0cfd41bee675c1b27761ed0364ea720f9
youngseo cho <dudtj0412@naver.com>
2023-12-15 11:22:52 +0900

    Feat: 플리 하나 조회 Repository 생성

--- src/main/java/com/example/Playlist_pack/Repository/PlaylistRepository.java
+++ src/main/java/com/example/Playlist_pack/Repository/PlaylistRepository.java
@@ -4,9 +4,11 @@ import com.example.Playlist_pack.Domain.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 
-
+    Optional<Playlist> findByPlaylistIdAndUserId(Long playlistId, Long userId);
     List<Playlist> findAllByUser_UserId(Long userId);
+
 }
