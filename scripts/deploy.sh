e4d415d2ce12ce89cd9f8b8fdd4bbf8863ef5e68
ulsandonghun <donghoon141@naver.com>
2023-12-16 18:29:18 +0900

    [fix] : deploy.log에 현재 구동중인 Process pid 간 나오도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "$(date)> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-echo "$(date)> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+echo "$(date)> 현재 구동중인 애플리케이션 pid: $CURRENT_PID" >> /home/ec2-user/action/deploy.log
 if [ -z $CURRENT_PID ]
 then
   echo "$(date)> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
