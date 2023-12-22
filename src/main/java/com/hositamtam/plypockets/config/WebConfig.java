3119080d301321f2a6a527de1f02ff14d6bcee13
안나 <dksek3050@gmail.com>
2023-12-22 21:57:32 +0900

    Fix: Cors 다시 전체허용으로 수정

--- src/main/java/com/hositamtam/plypockets/config/WebConfig.java
+++ src/main/java/com/hositamtam/plypockets/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // 원하는 도메인만 허용하도록 설정
-        config.addAllowedOrigin("https://www.plypockets.com");
+        config.addAllowedOrigin("*");
 
         // 허용할 헤더를 모두 허용하도록 설정
         config.addAllowedHeader("*");
