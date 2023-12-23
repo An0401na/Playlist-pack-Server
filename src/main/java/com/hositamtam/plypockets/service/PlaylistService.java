fc6b06e5f2b044c66bd045f22622a438ad6c9f56
안나 <dksek3050@gmail.com>
2023-12-23 14:08:18 +0900

    Fix: 회원가입시 첫 선물보낼 수 있도록 수정

--- src/main/java/com/hositamtam/plypockets/service/PlaylistService.java
+++ src/main/java/com/hositamtam/plypockets/service/PlaylistService.java
@@ -3,6 +3,9 @@ package com.hositamtam.plypockets.service;
 
 import com.hositamtam.plypockets.domain.Playlist;
 import com.hositamtam.plypockets.domain.User;
+import com.hositamtam.plypockets.domain.enums.ColorType;
+import com.hositamtam.plypockets.domain.enums.CoverType;
+import com.hositamtam.plypockets.domain.enums.DecoType;
 import com.hositamtam.plypockets.dto.request.PlaylistRegisterRequestDto;
 import com.hositamtam.plypockets.global.exception.custom.user.UserNotFoundException;
 import com.hositamtam.plypockets.repository.PlaylistRepository;
@@ -26,6 +29,19 @@ public class PlaylistService {
         User user = userRepository.findByNickname(nickname)
                 .orElseThrow(UserNotFoundException::new);
         playlistRepository.save(playlistRegisterRequestDto.toEntity(user));
+    }
+
+    public void createFirstPlaylist(User user) {
+        playlistRepository.save(Playlist.builder()
+                .coverIdx(CoverType.red)
+                .decoIdx(DecoType.ribbon)
+                .colorIdx(ColorType.white)
+                .letter("만나서 반가워요! 플리보따리를 이용해 주셔서 감사합니다💝 따뜻한 연말 되세요🎁")
+                .friendname("플리보따리🎁")
+                .spotifyId("0bYg9bo50gSsH3LtXe2SQn")
+                .user(user)
+                .build()
+        );
     }
 
     public List<Playlist> searchPlayListPackByNickname(String nickname) {
